b86e7e1c04996c1e406fef859c2dd9375724db48
GlobooX <skrinszot@wp.pl>
2019-03-06 20:18:41 +0100

    backup

--- app/src/main/java/pl/globoox/dinnerchecker/MainActivity.java
+++ app/src/main/java/pl/globoox/dinnerchecker/MainActivity.java
@@ -2,6 +2,13 @@ package pl.globoox.dinnerchecker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 
 import pl.globoox.dinnerchecker.Utilities.DownloadWebpage;
 
@@ -13,7 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        classWebPage.main(null);
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("http://www.piotr-lubawka.pl").get();
+                    Element masthead = doc.select("div.post-content").first();
+                    String dinner = masthead.text();
+
+                    Log.d("TAG", String.valueOf(dinner));
+
+
+                } catch (IOException e) {
+                    builder.append("Error : ").append(e.getMessage()).append("\n");
+                }
+
+            }
+        }).start();
+
+
+
     }
 
 
